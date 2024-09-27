da2512e38976390e1b1222e90fa7f70bef6ee432
Esteban Pedraza <estemanps99@gmail.com>
2024-09-27 13:40:45 -0500

    Aplicacion base funcionando

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './style'; // Este archivo incluye estilos globales.
-import App from './components/app';
-
-export default App;
---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import { h } from 'preact';
+import { hydrate, prerender as ssr } from 'preact-iso';
+import MateriasForm from './components/Materias/materiasForm';
+
+
+function App() {
+	return (
+	  <div className="container">
+		
+		<MateriasForm />
+	  </div>
+	);
+  }
+
+if (typeof window !== 'undefined') {
+	hydrate(<App />, document.getElementById('app'));
+}
+
+export async function prerender(data) {
+	return await ssr(<App {...data} />);
+}
